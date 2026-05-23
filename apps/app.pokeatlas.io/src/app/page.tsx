ad90fec6b3195e0fa88b465b21339feb05568bc2
Bram Adl <hello.bramadl@gmail.com>
2026-05-23 09:36:31 +0700

    feat(app): test poke atlas client facade for browse pokedex use case

--- apps/app.pokeatlas.io/src/app/page.tsx
+++ apps/app.pokeatlas.io/src/app/page.tsx
@@ -1,3 +1,18 @@
+import { atlas } from "@pokeatlas/core";
+
 export default async function Home() {
-	return <main>Hello World</main>;
+	const result = await atlas.collection.browsePokedex({
+		trainerId: "00000000-0000-0000-0000-000000000001",
+	});
+
+	return (
+		<main>
+			<p>Showing {result.value().totalEntries} pokemon</p>
+			<div className="grid grid-cols-6 gap-4 p-10">
+				{result.value().entries.map((pokemon) => (
+					<div key={pokemon.id}>{pokemon.name}</div>
+				))}
+			</div>
+		</main>
+	);
 }
