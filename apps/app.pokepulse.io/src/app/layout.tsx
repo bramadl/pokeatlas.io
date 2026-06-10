ea9f16f8a01ce3fffc2b8eca6ea91664fca47f52
Bram Adl <hello.bramadl@gmail.com>
2026-06-10 14:25:35 +0700

    fix: remove code noise

--- apps/app.pokepulse.io/src/app/layout.tsx
+++ apps/app.pokepulse.io/src/app/layout.tsx
@@ -3,7 +3,6 @@ import { IBM_Plex_Mono, Lora, Plus_Jakarta_Sans } from "next/font/google";
 
 import { GlobalFooter } from "@/components/global/footer";
 import { GlobalNavigation } from "@/components/global/navigation";
-// import { ThemeProvider } from "@/components/global/theme-provider";
 import { GlobalProgressBar } from "@/components/ui/progress-bar";
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
